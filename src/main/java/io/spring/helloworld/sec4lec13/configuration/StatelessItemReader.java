c8f6d4eb1df2a20b11c5a81c9a35b62f42cd642c
cookker <cookker@something.todo>
2018-07-28 17:28:30 +0900

    sec3lec12 stateless itemReader

---
+++ src/main/java/io/spring/helloworld/sec4lec13/configuration/StatelessItemReader.java
@@ -0,0 +1,22 @@
+package io.spring.helloworld.sec4lec13.configuration;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class StatelessItemReader implements ItemReader<String> {
+    private final Iterator<String> data;
+
+    public StatelessItemReader(List<String> data) {
+        this.data = data.iterator();
+    }
+
+    @Override
+    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        return this.data.hasNext() ? this.data.next() : null;
+    }
+}
