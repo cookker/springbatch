96632bebfb22815be9270fb5cf316a97abe192c2
cookker <cookker@something.todo>
2018-07-29 00:15:53 +0900

    sec4lec15-csv

--- src/main/java/io/spring/helloworld/sec4lec14/JobConfiguration.java
+++ src/main/java/io/spring/helloworld/sec4lec14/JobConfiguration.java
@@ -1,78 +1,78 @@
-package io.spring.helloworld.sec4lec14;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import java.util.Collections;
-
-@Configuration
-@Slf4j
-@RequiredArgsConstructor
-public class JobConfiguration {
-    private final StepBuilderFactory stepBuilderFactory;
-    private final JobBuilderFactory jobBuilderFactory;
-    private final DataSource dataSource;
-
-    @Bean
-    public JdbcCursorItemReader<Customer> cursorItemReader(){
-        JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<>();
-        reader.setSql("select id, firstName, lastName, birthdate from customer order by lastName, firstName");
-        reader.setDataSource(dataSource);
-        reader.setRowMapper(new CustomerRowMapper());
-        return reader;
-    }
-
-    @Bean
-    public JdbcPagingItemReader<Customer> pagingItemReader(){
-        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
-        reader.setDataSource(dataSource);
-        reader.setFetchSize(10);
-        reader.setRowMapper(new CustomerRowMapper());
-
-        MySqlPagingQueryProvider pagingQueryProvider = new MySqlPagingQueryProvider();
-        pagingQueryProvider.setSelectClause("id, firstName, lastName, birthdate");
-        pagingQueryProvider.setFromClause("from customer");
-        pagingQueryProvider.setSortKeys(Collections.singletonMap("id", Order.DESCENDING));
-
-        reader.setQueryProvider(pagingQueryProvider);
-        return reader;
-    }
-
-    @Bean
-    public ItemWriter<Customer> customerItemWriter(){
-        return items -> {
-          items.forEach(i -> log.info("current Item: {}", i));
-        };
-    }
-
-    @Bean
-    public Step step1(){
-        return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(10)
-                .reader(pagingItemReader())
-//                .reader(cursorItemReader())
-                .writer(customerItemWriter())
-                .allowStartIfComplete(true)
-                .build();
-    }
-
-    @Bean
-    public Job job(){
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .build();
-    }
-
-}
+//package io.spring.helloworld.sec4lec14;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.item.ItemWriter;
+//import org.springframework.batch.item.database.JdbcCursorItemReader;
+//import org.springframework.batch.item.database.JdbcPagingItemReader;
+//import org.springframework.batch.item.database.Order;
+//import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//import java.util.Collections;
+//
+//@Configuration
+//@Slf4j
+//@RequiredArgsConstructor
+//public class JobConfiguration {
+//    private final StepBuilderFactory stepBuilderFactory;
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final DataSource dataSource;
+//
+//    @Bean
+//    public JdbcCursorItemReader<Customer> cursorItemReader(){
+//        JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<>();
+//        reader.setSql("select id, firstName, lastName, birthdate from customer order by lastName, firstName");
+//        reader.setDataSource(dataSource);
+//        reader.setRowMapper(new CustomerRowMapper());
+//        return reader;
+//    }
+//
+//    @Bean
+//    public JdbcPagingItemReader<Customer> pagingItemReader(){
+//        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+//        reader.setDataSource(dataSource);
+//        reader.setFetchSize(10);
+//        reader.setRowMapper(new CustomerRowMapper());
+//
+//        MySqlPagingQueryProvider pagingQueryProvider = new MySqlPagingQueryProvider();
+//        pagingQueryProvider.setSelectClause("id, firstName, lastName, birthdate");
+//        pagingQueryProvider.setFromClause("from customer");
+//        pagingQueryProvider.setSortKeys(Collections.singletonMap("id", Order.DESCENDING));
+//
+//        reader.setQueryProvider(pagingQueryProvider);
+//        return reader;
+//    }
+//
+//    @Bean
+//    public ItemWriter<Customer> customerItemWriter(){
+//        return items -> {
+//          items.forEach(i -> log.info("current Item: {}", i));
+//        };
+//    }
+//
+//    @Bean
+//    public Step step1(){
+//        return stepBuilderFactory.get("step1")
+//                .<Customer, Customer>chunk(10)
+//                .reader(pagingItemReader())
+////                .reader(cursorItemReader())
+//                .writer(customerItemWriter())
+//                .allowStartIfComplete(true)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Job job(){
+//        return jobBuilderFactory.get("job")
+//                .start(step1())
+//                .build();
+//    }
+//
+//}
---
+++ src/main/java/io/spring/helloworld/sec4lec15/JobConfiguration.java
@@ -0,0 +1,66 @@
+package io.spring.helloworld.sec4lec15;
+
+import io.spring.helloworld.sec4lec14.Customer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class JobConfiguration {
+    private final StepBuilderFactory stepBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
+
+    @Bean
+    public FlatFileItemReader<Customer> customerItemReader(){
+        FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
+
+        reader.setLinesToSkip(1);
+        reader.setResource(new ClassPathResource("/data/customer.csv"));
+
+        DefaultLineMapper<Customer> customerLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames(new String[]{"id", "firstName", "lastName", "birthdate"});
+
+        customerLineMapper.setLineTokenizer(tokenizer);
+        customerLineMapper.setFieldSetMapper(new CustomerFieldSetMapper());
+        customerLineMapper.afterPropertiesSet();
+
+        reader.setLineMapper(customerLineMapper);
+        return reader;
+    }
+
+    @Bean
+    public ItemWriter<Customer> customerItemWriter(){
+        return items -> {
+          items.forEach(i -> log.info("current Item:{}", i));
+        };
+    }
+
+    @Bean
+    public Step step1(){
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(10)
+                .reader(customerItemReader())
+                .writer(customerItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job jobCsv(){
+        return jobBuilderFactory.get("jobCsv")
+                .start(step1())
+                .build();
+    }
+}
