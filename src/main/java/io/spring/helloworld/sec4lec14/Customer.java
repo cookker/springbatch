95e9c7238b510f146664ea81e59de8cf15eda59f
cookker <cookker@something.todo>
2018-07-29 17:59:47 +0900

    itemReader with state

--- src/main/java/io/spring/helloworld/sec4lec14/Customer.java
+++ src/main/java/io/spring/helloworld/sec4lec14/Customer.java
@@ -1,15 +1,15 @@
-package io.spring.helloworld.sec4lec14;
-
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
-
-import java.util.Date;
-
-@RequiredArgsConstructor
-@ToString
-public class Customer {
-    private final long id;
-    private final String firstName;
-    private final String lastName;
-    private final Date birthdate;
-}
+//package io.spring.helloworld.sec4lec14;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.ToString;
+//
+//import java.util.Date;
+//
+//@RequiredArgsConstructor
+//@ToString
+//public class Customer {
+//    private final long id;
+//    private final String firstName;
+//    private final String lastName;
+//    private final Date birthdate;
+//}
---
+++ src/main/java/io/spring/helloworld/sec4lec19/StatefullItemReader.java
@@ -0,0 +1,56 @@
+package io.spring.helloworld.sec4lec19;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.*;
+
+import java.util.List;
+
+@Slf4j
+public class StatefullItemReader implements ItemStreamReader<String> {
+    private final List<String> items;
+    private int curIndex = -1;
+    private boolean restart = false;
+
+    public StatefullItemReader(List<String> items) {
+        this.items = items;
+        this.curIndex = 0;
+    }
+
+    @Override
+    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        String item = null;
+
+        if(this.curIndex < this.items.size()){
+            item = this.items.get(this.curIndex);
+            this.curIndex++;
+        }
+
+        if(this.curIndex == 46 && !restart){
+            throw new RuntimeException("일부러 에러를 내보세.");
+
+        }
+        return item;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        log.info("open호출, curIndex:{}", curIndex);
+        if(executionContext.containsKey("curIndex")){
+            this.curIndex = executionContext.getInt("curIndex");
+            this.restart = true;
+        }else{
+            this.curIndex = 0;
+            executionContext.put("curIndex", this.curIndex);
+        }
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        log.info("update호출, curIndex:{}", curIndex);
+        executionContext.put("curIndex", this.curIndex);
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+    }
+}
